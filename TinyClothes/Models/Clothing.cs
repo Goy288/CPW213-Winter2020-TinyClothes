dc2ea2e4ecf6c6c3fa9c1f63d1b1a2c9a997fd91
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-04 10:16:39 -0800

    Added some validation to the Clothing class.

--- TinyClothes/Models/Clothing.cs
+++ TinyClothes/Models/Clothing.cs
@@ -17,29 +17,40 @@ namespace TinyClothes.Models
         /// <summary>
         /// The clothing's size,
         /// </summary>
+        [Required(ErrorMessage = "Size is required")]
         public string Size { get; set; }
 
         /// <summary>
         /// The type of clothing.
         /// </summary>
+        [Required]
         public string Type { get; set; }
 
         // The clothing's color.
+        [Required]
         public string Color { get; set; }
 
         /// <summary>
         /// Retail price of the item
         /// </summary>
+        [Required]
+        [Range(0.00, 1000000000.00)]
         public double Price { get; set; }
 
         /// <summary>
         /// The clothing's display title.
         /// </summary>
+        [Required]
+        [StringLength(32)]
+        // Sample Regex, great for validation.
+        //[RegularExpression("^([A-Za-z0-9])+$",
+        // ErrorMessage = "lmao error")]
         public string Title { get; set; }
 
         /// <summary>
         /// Describes the clothing.
         /// </summary>
+        [StringLength(1024)]
         public string Description { get; set; }
 
     }
