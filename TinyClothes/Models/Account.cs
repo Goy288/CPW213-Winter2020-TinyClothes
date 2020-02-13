7fef77310f312c98903aa58d87310c915a47e28e
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-13 09:20:37 -0800

    Created RegistryViewModel

--- TinyClothes/Models/Account.cs
+++ TinyClothes/Models/Account.cs
@@ -39,7 +39,7 @@ namespace TinyClothes.Models
         public string Username { get; set; }
 
         /// <summary>
-        /// The passwrod for logging in
+        /// The password for logging in
         /// </summary>
         [Required]
         [StringLength(256)]
@@ -65,5 +65,53 @@ namespace TinyClothes.Models
         /// </summary>
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+    }
+
+    public class RegistryViewModel
+    {
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        [Required]
+        [StringLength(64)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        [Required]
+        [StringLength(64)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The username
+        /// </summary>
+        [Required]
+        [StringLength(32)]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password for logging in
+        /// </summary>
+        [Required]
+        [StringLength(256)]
+        [DataType(DataType.Password)] // <input type="password">
+        public string Password { get; set; }
+
+        /// <summary>
+        /// The password for logging in
+        /// </summary>
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)] // <input type="password">
+        public string ConfirmPassword { get; set; }
+        
+        /// <summary>
+        /// The email to send shipping notices to.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
