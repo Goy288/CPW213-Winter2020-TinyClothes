2fcfa79983354a79488d41e9dd0f02bd0a4ba0fb
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-25 10:31:53 -0800

    Made a couple of classes.

---
+++ TinyClothes/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly StoreContext _context;
+        private readonly IHttpContextAccessor _accessor;
+
+        public CartController(StoreContext context, IHttpContextAccessor accessor)
+        {
+            this._context = context;
+            this._accessor = accessor;
+        }
+
+        // Displate all products in cart
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Add a single product to the shopping cart
+        public async Task<IActionResult> Add(int id)
+        {
+            Clothing c = await ClothingDB.GetClothingbyID(id, _context);
+
+            if (c != null)
+            {
+                CartHelper.Add(c, );
+            }
+
+            return View();
+        }
+
+        // Summary checkout
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
---
+++ TinyClothes/Models/CartHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Helper class to manage users
+    /// shopping cart data using cookies.
+    /// </summary>
+    public static class CartHelper
+    {
+        public static void Add(Clothing c, IHttpContextAccessor accessor)
+        {
+            string data = JsonConvert.SerializeObject(c);                                                               );
+
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(14),
+                IsEssential = true,
+                Secure = true
+            };
+
+            accessor.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+        }
+
+        public static int GetCount(IHttpContextAccessor accessor)
+        {
+            throw new NotImplementedException();
+        }
+        public static List<Clothing> GetAllClothes(IHttpContextAccessor accessor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
