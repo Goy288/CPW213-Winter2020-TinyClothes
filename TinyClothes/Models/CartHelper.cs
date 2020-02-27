70cff768e099bd78e6de781f006a7c94209153d9
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-02-27 10:58:56 -0800

    User can now add multiple cart items, and can now see a cart icon displaying the item amount.

--- TinyClothes/Models/CartHelper.cs
+++ TinyClothes/Models/CartHelper.cs
@@ -13,9 +13,14 @@ namespace TinyClothes.Models
     /// </summary>
     public static class CartHelper
     {
+        private const string CartCookie = "CartCookie";
+
         public static void Add(Clothing c, IHttpContextAccessor accessor)
         {
-            string data = JsonConvert.SerializeObject(c);
+            List<Clothing> clothes = GetAllClothes(accessor);
+            clothes.Add(c);
+
+            string data = JsonConvert.SerializeObject(clothes);
 
             CookieOptions options = new CookieOptions()
             {
@@ -24,16 +29,25 @@ namespace TinyClothes.Models
                 Secure = true
             };
 
-            accessor.HttpContext.Response.Cookies.Append("CartCookie", data, options);
+            accessor.HttpContext.Response.Cookies.Append(CartCookie, data, options);
         }
 
         public static int GetCount(IHttpContextAccessor accessor)
-        {
-            throw new NotImplementedException();
-        }
+            => GetAllClothes(accessor).Count;
+
+        /// <summary>
+        /// Returns all clothing currently stored in the user's cookie.
+        /// If no items are present, then an empty list is returned.
+        /// </summary>
+        /// <param name="accessor"></param>
+        /// <returns></returns>
         public static List<Clothing> GetAllClothes(IHttpContextAccessor accessor)
         {
-            throw new NotImplementedException();
+            string data = accessor.HttpContext.Request.Cookies[CartCookie];
+
+            return string.IsNullOrWhiteSpace(data) ? 
+                   new List<Clothing>() : 
+                   JsonConvert.DeserializeObject<List<Clothing>>(data);
         }
     }
 }
