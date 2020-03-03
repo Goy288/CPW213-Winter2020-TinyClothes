69e1ee263698724df26a87268f41ae3ff26e808c
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-03-03 10:22:27 -0800

    Made a new SearchCriteria class.

---
+++ TinyClothes/Models/SearchCriteria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    public class SearchCriteria
+    {
+        public SearchCriteria()
+        {
+            Results = new List<Clothing>();
+        }
+
+        public string Size { get; set; }
+
+        /// <summary>
+        /// The type of clothing.
+        /// </summary>
+        public string Type { get; set; }
+
+        public double? MinPrice { get; set; }
+
+        public double? MaxPrice { get; set; }
+
+        public List<Clothing> Results { get; set; }
+    }
+}
